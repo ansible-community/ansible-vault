d179b74e3114acec9a29ea5ebc5cc7f4f524a1bd
Krass, Leon <leon.krass@sva.de>
2026-09-10 14:23:50 +0200

    chore: update molecule configurations to seperate os and functional tests

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
         default = pkgs.mkShell {
           venvDir = ".venv";
           packages = with pkgs;
-            [ ansible ansible-lint python312 ]
+            [ ansible ansible-lint molecule python312 ]
             ++ (with pkgs.python312Packages; [ pip venvShellHook ]);
         };
       });
